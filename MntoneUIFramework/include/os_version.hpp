9eea288d76b1393b49407341bbc27828a908821d
mntone <sd8@live.jp>
2015-07-06 16:30:50 +0900

    Improve OS check.
    Error check and bug fix.

--- MntoneUIFramework/include/os_version.hpp
+++ MntoneUIFramework/include/os_version.hpp
@@ -10,68 +10,72 @@ public:
 	enum os_label { unknown = 0, before = 49, two_thousand = 50, xp, vista = 60, seven, eight, eight_one, ten, later };
 
 	os_version() noexcept
+		: os_version_label_(os_label::unknown)
+		, workstation_(false)
 	{
-		OSVERSIONINFOW os_version = { sizeof(OSVERSIONINFOW) };
-		GetVersionExW(&os_version);
+		OSVERSIONINFOEXW os_version = { sizeof(OSVERSIONINFOEXW) };
+		BOOL b = GetVersionExW(reinterpret_cast<OSVERSIONINFOW*>(&os_version));
+		if (b != 0)
+		{
+			if (os_version.dwMajorVersion == 10)
+			{
+				switch (os_version.dwMinorVersion)
+				{
+				case 0:
+					os_version_label_ = os_label::ten;
+					break;
+				default:
+					os_version_label_ = os_label::unknown;
+					break;
+				}
+			}
+			else if (os_version.dwMajorVersion > 6)
+			{
+				os_version_label_ = os_label::unknown;
+			}
+			else if (os_version.dwMajorVersion == 6)
+			{
+				switch (os_version.dwMinorVersion)
+				{
+				case 3:
+					os_version_label_ = os_label::eight_one;
+					break;
+				case 2:
+					os_version_label_ = os_label::eight;
+					break;
+				case 1:
+					os_version_label_ = os_label::seven;
+					break;
+				case 0:
+					os_version_label_ = os_label::vista;
+					break;
+				default:
+					os_version_label_ = os_label::unknown;
+					break;
+				}
+			}
+			else if (os_version.dwMajorVersion == 5)
+			{
+				switch (os_version.dwMinorVersion)
+				{
+				case 1:
+					os_version_label_ = os_label::xp;
+					break;
+				case 0:
+					os_version_label_ = os_label::two_thousand;
+					break;
+				default:
+					os_version_label_ = os_label::unknown;
+					break;
+				}
+			}
+			else
+			{
+				os_version_label_ = os_label::before;
+			}
 
-		if (os_version.dwMajorVersion == 10)
-		{
-			switch (os_version.dwMinorVersion)
-			{
-			case 0:
-				os_version_label_ = os_label::ten;
-				break;
-			default:
-				os_version_label_ = os_label::unknown;
-				break;
-			}
+			workstation_ = os_version.wProductType == VER_NT_WORKSTATION;
 		}
-		else if (os_version.dwMajorVersion > 6)
-		{
-			os_version_label_ = os_label::unknown;
-		}
-		else if (os_version.dwMajorVersion == 6)
-		{
-			switch (os_version.dwMinorVersion)
-			{
-			case 3:
-				os_version_label_ = os_label::eight_one;
-				break;
-			case 2:
-				os_version_label_ = os_label::eight;
-				break;
-			case 1:
-				os_version_label_ = os_label::seven;
-				break;
-			case 0:
-				os_version_label_ = os_label::vista;
-				break;
-			default:
-				os_version_label_ = os_label::unknown;
-				break;
-			}
-		}
-		else if (os_version.dwMajorVersion == 5)
-		{
-			switch (os_version.dwMinorVersion)
-			{
-			case 1:
-				os_version_label_ = os_label::xp;
-				break;
-			case 0:
-				os_version_label_ = os_label::two_thousand;
-				break;
-			default:
-				os_version_label_ = os_label::unknown;
-				break;
-			}
-		}
-		else
-		{
-			os_version_label_ = os_label::before;
-		}
-
-		workstation_ = os_version.dwPlatformId == VER_NT_WORKSTATION;
 	}
 
 	bool operator==(os_label const& rhs) const noexcept { return os_version_label_ == rhs; }
